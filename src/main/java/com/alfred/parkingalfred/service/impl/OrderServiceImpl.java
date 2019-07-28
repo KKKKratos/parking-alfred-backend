20fb18f92faf88befa7c6af072d0b62f3c5be91d
zhanggi3 <gio.zhang@oocl.com>
2019-07-28 22:09:54 +0800

    [gio]:[feat,refactor:SecKillOrder of orderServiceImpl and it's test,add lock deal with the concurrence]

--- src/main/java/com/alfred/parkingalfred/service/impl/OrderServiceImpl.java
+++ src/main/java/com/alfred/parkingalfred/service/impl/OrderServiceImpl.java
@@ -6,9 +6,12 @@ import com.alfred.parkingalfred.entity.Order;
 import com.alfred.parkingalfred.enums.OrderStatusEnum;
 import com.alfred.parkingalfred.enums.ResultEnum;
 import com.alfred.parkingalfred.exception.OrderNotExistedException;
+import com.alfred.parkingalfred.exception.SecKillOrderException;
 import com.alfred.parkingalfred.repository.OrderRepository;
 import com.alfred.parkingalfred.service.OrderService;
+import com.alfred.parkingalfred.utils.RedisLock;
 import com.alfred.parkingalfred.utils.UUIDUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,49 +19,69 @@ import java.util.List;
 @Service
 public class OrderServiceImpl implements OrderService {
 
-    private final OrderRepository orderRepository;
+  @Autowired
+  private RedisLock redisLock;
+  private static final int TIMEOUT = 10 * 1000;//超时时间 10s
 
-    public OrderServiceImpl(OrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
+  private final OrderRepository orderRepository;
+
+  public OrderServiceImpl(OrderRepository orderRepository) {
+    this.orderRepository = orderRepository;
+  }
+
+  @Override
+  public Order addOrder(CreateOrderDto createOrderDto) {
+
+    Order order = mapToOrder(createOrderDto);
+    return orderRepository.save(order);
+  }
+
+  @Override
+  public Order getOrderById(Long id) {
+    return orderRepository.findById(id)
+        .orElseThrow(() -> new OrderNotExistedException(ResultEnum.RESOURCES_NOT_EXISTED));
+  }
+
+  @Override
+  public Order updateOrderStatusById(Long id, Order order) {
+
+    //lock
+    long time = System.currentTimeMillis() + TIMEOUT;
+    if (!redisLock.lock(id.toString(), String.valueOf(time))) {
+      throw new SecKillOrderException(ResultEnum.RESOURCES_NOT_EXISTED);
     }
 
-    @Override
-    public Order addOrder(CreateOrderDto createOrderDto) {
-        Order order = mapToOrder(createOrderDto);
-        return orderRepository.save(order);
+    Order orderFinded = orderRepository.findById(id)
+        .orElseThrow(() -> new OrderNotExistedException(ResultEnum.RESOURCES_NOT_EXISTED));
+    Integer statusResult = orderFinded.getStatus();
+    if (statusResult.equals(OrderStatusEnum.CONFIRM)){
+      throw new SecKillOrderException(ResultEnum.RESOURCES_NOT_EXISTED);
+    }else{
+      orderFinded.setStatus(order.getStatus());
     }
+    //unlock
+    redisLock.unlock(id.toString(),String.valueOf(time));
+    return orderRepository.save(orderFinded);
+  }
 
-    @Override
-    public Order getOrderById(Long id) {
-        return orderRepository.findById(id).orElseThrow(() -> new OrderNotExistedException(ResultEnum.RESOURCES_NOT_EXISTED));
-    }
+  @Override
+  public List<Order> getOrders() {
+    return orderRepository.findAll();
+  }
 
-    @Override
-    public Order updateOrderStatusById(Long id, Order order) {
-        Order orderFinded = orderRepository.findById(id)
-                .orElseThrow(() -> new OrderNotExistedException(ResultEnum.RESOURCES_NOT_EXISTED));
-        orderFinded.setStatus(order.getStatus());
-        return orderRepository.save(orderFinded);
-    }
+  private Order mapToOrder(CreateOrderDto createOrderDto) {
+    Order order = new Order();
+    order.setCarNumber(createOrderDto.getCarNumber());
+    order.setType(createOrderDto.getType());
+    order.setCustomerAddress(createOrderDto.getCustomerAddress());
+    order.setReservationTime(createOrderDto.getReservationTime());
+    order.setStatus(OrderStatusEnum.WAIT_FOR_RECEIVE.getCode());
+    order.setOrderId(UUIDUtil.generateUUID());
+    return order;
+  }
 
-    @Override
-    public List<Order> getOrders() {
-        return orderRepository.findAll();
-    }
-
-    private Order mapToOrder(CreateOrderDto createOrderDto) {
-        Order order = new Order();
-        order.setCarNumber(createOrderDto.getCarNumber());
-        order.setType(createOrderDto.getType());
-        order.setCustomerAddress(createOrderDto.getCustomerAddress());
-        order.setReservationTime(createOrderDto.getReservationTime());
-        order.setStatus(OrderStatusEnum.WAIT_FOR_RECEIVE.getCode());
-        order.setOrderId(UUIDUtil.generateUUID());
-        return order;
-    }
-    
-    @Override
-    public List<Order> getOrdersByTypeAndStatus(Integer type,Integer status){
-        return orderRepository.findOrdersByTypeAndStatus(type,status);
-    }
+  @Override
+  public List<Order> getOrdersByTypeAndStatus(Integer type, Integer status) {
+    return orderRepository.findOrdersByTypeAndStatus(type, status);
+  }
 }
