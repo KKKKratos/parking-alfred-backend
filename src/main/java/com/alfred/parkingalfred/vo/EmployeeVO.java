2168992c92c24809f6315ba6977f13d9f2d2e086
zhanggi3 <gio.zhang@oocl.com>
2019-07-29 13:24:07 +0800

    [gio]:[refactor:EmployeeToEmployeeVOConverter]

--- src/main/java/com/alfred/parkingalfred/vo/EmployeeVO.java
+++ src/main/java/com/alfred/parkingalfred/vo/EmployeeVO.java
@@ -1,5 +1,25 @@
 package com.alfred.parkingalfred.vo;
 
-public class EmployeeVO {
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import lombok.Data;
+
+@Data
+public class EmployeeVO implements Serializable {
+  private static final long serialVersionUID = 5585749034096419420L;
+
+  private Long id;
+
+  private String name;
+
+  private String telephone;
+
+  private Integer status;
+
+  private String mail;
+
+  private String password;
+
+  private Integer role;
 }
