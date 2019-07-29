d409a1c04009f352f4e22739116c99888c0841cb
曹桂凤 <demi.cao@oocl.com>
2019-07-29 20:36:34 +0800

    [Demi][feat:fix the updateOrderStatusById API]

--- src/main/java/com/alfred/parkingalfred/service/impl/OrderServiceImpl.java
+++ src/main/java/com/alfred/parkingalfred/service/impl/OrderServiceImpl.java
@@ -53,12 +53,12 @@ public class OrderServiceImpl implements OrderService {
         .orElseThrow(() -> new OrderNotExistedException(ResultEnum.RESOURCES_NOT_EXISTED));
     Integer statusResult = orderFinded.getStatus();
     if (order.getStatus().equals(OrderStatusEnum.CONFIRM)){
-      orderFinded.setStatus(order.getStatus());
+      this.updateOrder(orderFinded,order);
     }else{
       if (statusResult.equals(OrderStatusEnum.WAIT_FOR_CONFIRM)) {
         throw new SecKillOrderException(ResultEnum.RESOURCES_NOT_EXISTED);
       } else {
-        orderFinded.setStatus(order.getStatus());
+        this.updateOrder(orderFinded,order);
       }
       //unlock
       redisLock.unlock(id.toString(), String.valueOf(time));
@@ -85,5 +85,29 @@ public class OrderServiceImpl implements OrderService {
   @Override
   public List<Order> getOrdersByStatus(Integer status) {
     return orderRepository.findOrdersByStatus(status);
+  }
+
+  private void updateOrder(Order orderFinded, Order order){
+      if (order.getType() != null) {
+          orderFinded.setStatus(order.getType());
+      }
+      if (order.getReservationTime() != null) {
+          orderFinded.setReservationTime(order.getReservationTime());
+      }
+      if (order.getCustomerAddress() != null) {
+          orderFinded.setCustomerAddress(order.getCustomerAddress());
+      }
+      if (order.getStatus() != null) {
+          orderFinded.setStatus(order.getStatus());
+      }
+      if (order.getEmployee() != null) {
+          orderFinded.setEmployee(order.getEmployee());
+      }
+      if (order.getCarNumber() != null) {
+          orderFinded.setCarNumber(order.getCarNumber());
+      }
+      if (order.getParkingLot() != null) {
+          orderFinded.setParkingLot(order.getParkingLot());
+      }
   }
 }
